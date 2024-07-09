482f4485cc8df66601f1294712e1f1a48abf9594
Dan Norman <buzzcutnorman@gmail.com>
2024-07-09 09:40:00 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Tuva Seed Reader #
+This little application is to be used in conjunction with The Tuva Project DBT package when utilizing PostgesSQL for you datawarehouse.  The Tuva Seed Reader allows you to override the load_seed macro with a PostgreSQL version that leverages COPY FROM PROGRAM.  You guessed it the program that you are copying the data from is the Tuva Seed Reader.  The same URI and PATTERN argumanets passed in the post-hook calls found in The Tuva Project dbt_project.yml file.
+
+## How Get it Working ##
+You need to complete two task in order to get the Tuva Seed Reader and working with the Tuva data project.
+
+1) Install the tuva seed reader on you PostgresSQL server.
+2) Override the load_macro macro in your DBT Project.
+
+## Installing Tuva Seed Reader ##
+Windows does not have a global installtion for pipx so you need to run the PostgreSQL service under a local or domain account.  When you connect you will need to connect at this service users so you can complete the following steps.
+
+Connect to your PostgreSQL Server and complete these steps:
+
+1. [Install python 3.9 or greater](https://www.python.org/downloads/). (I tested mainly with 3.12)
+2. Connect to the server as the service user.
+3. [Install pipx](https://pipx.pypa.io/latest/installation/).
+    ```bash
+    py -m pip install --user pipx
+    ```
+4. Ensure pipx path.
+    ```bash
+    pipx ensurepath
+    ```
+5. Install tuva seed reader.
+    ```bash
+    pipx install git+https://github.com/BuzzCutNorman/tuva-seed-reader.git
+    ```
+
+## Override the load_seed Macro ##
+In your DBT project folder open your dbt_project.yml and andd the below dispatch: .  Change the placeholder 'your_project_name_here' to you project's macro name space.  If you do not have, one use the name of your project.  That is what I did and it seems to work nicely.
+
+```
+dispatch:
+  - macro_namespace: the_tuva_project
+    search_order: ['your_project_name_here','the_tuva_project']
+```
+
+In your DBT project folder, create a macros forlder if you do not already have one.  In that folder create a new file and name it `load_seed.sql`.  Cut and paste in the below code and save the file.
+
+```
+{% macro postgres__load_seed(uri,pattern,compression,headers,null_marker) %}
+
+{% set sql %}
+COPY {{ this }}
+  FROM PROGRAM 'tuva-seed-reader {{ uri }} {{ pattern }}'
+  WITH (FORMAT CSV{% if headers == True %}, HEADER MATCH{% endif %})
+{% endset %}
+
+{% if execute %}
+{# debugging { log(sql, True)} #}
+{% set results = run_query(sql) %}
+{{ log("Loaded data from external s3 resource\n  loaded to: " ~ this ~ "\n  from: s3://" ~ uri ,True) }}
+{# debugging { log(results, True) } #}
+{% endif %}
+
+{% endmacro %}
+```
+
+## Testing ##
+I would suggest running `dbt seed` or `dbt build`.  Hopefully you will see all the terminology, reference, and valueset data get seeded properly.  If you have, a failure and you want to see more in the logs you can add `--log-level debug` to the dbt command you used.
---
+++ tuva_seed_reader/reader.py
@@ -0,0 +1,90 @@
+"""Reads Tuva S3 Files for Postgres"""
+import boto3
+import sys
+import typer
+from smart_open import open
+
+
+def parse_uri(uri:str) -> tuple[str,str]: 
+    """ Parses the uri into bucket base and object path to find the csv files.
+
+        Args:
+            uri: string the is in the format 'bucket/path/to/object'
+
+        Returns:
+            Two string the first is the bucket_name
+            the second is the object_path used to filter by prefix
+    """
+    parse_list = uri.split("/",1)
+    return parse_list[0],parse_list[1]
+
+def get_bucket_contents(client,bucket:str,prefix:str) -> dict:
+    """ Obtains all the contents in the bucket filtered by prefix
+
+        Args:
+            client: S3 client object connected to tuva S3 bucket.
+            bucket: The name of the tuva bucket to search.
+            prefix: Object path used to fileter the bucket search.
+
+        Returns:
+            The "Contents" section of the result in a dictionary format or None
+    """
+    result = client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    return result.get("Contents",{})
+
+def get_s3_objs_by_pattern(contents:dict,pattern:str) -> list[str]:
+    """Find all the objects that match the pattern in the bucket contents dictionary.
+    
+        Args:
+            contents: Dictionary containing the buckets contents info to search.
+            pattern: The key pattern of the csv file to seach for.
+        
+        Returns:
+            A list of files found that matched the pattern.
+    """
+    result_list = []
+    for obj in contents:
+        if str(obj["Key"]).startswith(pattern):
+                result_list.append(obj)
+    return result_list 
+
+def main(uri:str, pattern:str)  -> None:
+    """Main loop that does the following.
+        
+        1. Creats a S3 Client object with the Tuva access info.
+        2. Parses the URI given to pass to other function.
+        3. Obtains the contents of the bucket filtered to a specific path.
+        4. Finds all the files that match the pattern in the contents and
+           writes there contents out to stdout. 
+    
+    """
+    # The Tuva Project S3 Access Info
+    tuva_region_name:str = "us-east-1"
+    tuva_aws_access_key_id:str="AKIA2EPVNTV4FLAEBFGE"
+    tuva_aws_secret_access_key:str="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"
+
+    # AWS S3 client object to be used
+    tuva_s3_client = boto3.client(
+        service_name="s3",
+        region_name=tuva_region_name,
+        aws_access_key_id=tuva_aws_access_key_id,
+        aws_secret_access_key=tuva_aws_secret_access_key,
+    )
+
+    # Populate variable neccary to read the contents of the Tuva seed files.
+    bucket_name,object_path = parse_uri(uri=uri)
+    key_pattern = f"{object_path}/{pattern}"
+    bucket_contents = get_bucket_contents(tuva_s3_client,bucket=bucket_name,prefix=object_path)
+    
+    # Read the contents of the Tuva seed files passing the stdout so COPY FROM PROGRAM grab them.
+    for obj in get_s3_objs_by_pattern(contents=bucket_contents,pattern=key_pattern):
+        for line in open(f"s3://{bucket_name}/{obj['Key']}",mode="rb",transport_params={"client":tuva_s3_client}):
+            sys.stdout.buffer.write(line.replace(b'"\\N"',b''))
+            sys.stdout.flush()  
+
+def typer_run_main() -> None:
+    """Wrapper function to allow main to be stared by Typer."""
+    typer.run(main)
+
+if __name__ == "__main__":
+    typer_run_main
